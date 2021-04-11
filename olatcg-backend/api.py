24ee2f8fe92adc4267e1ccf27606c5dfd7e211d4
Wesley Ferreira <ovvesley@protonmail.com>
2021-04-11 19:56:02 -0300

    Cors: habilitando cors para todos os dominios

--- olatcg-backend/api.py
+++ olatcg-backend/api.py
@@ -1,8 +1,11 @@
 from flask import Flask, Blueprint
 from view.seqAlignmentView import seqAlignmentView
 from view.homologySearchView import homologySearchView
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(seqAlignmentView)
 app.register_blueprint(homologySearchView)
