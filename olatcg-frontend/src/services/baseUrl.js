3363cbf5f9e6043463dc21b283b11448730ca076
luiz.viana <luiz.viana@radixeng.com.br>
2021-04-15 13:14:25 -0300

    modificando baseUrl para novo endereço do backend

--- olatcg-frontend/src/services/baseUrl.js
+++ olatcg-frontend/src/services/baseUrl.js
@@ -1,1 +1,1 @@
-export default process.env.REACT_APP_BACKEND_URL|| "https://olatcg-backend-n.herokuapp.com";
+export default process.env.REACT_APP_BACKEND_URL|| "https://olatcg-backend.herokuapp.com";
